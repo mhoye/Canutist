21f01fce2cc6baec8741876facb48f4079bc0629
joeyk <jkrejci@mozilla.com>
2017-10-11 13:18:40 -0700

    Updated Nubis version 2.0.0

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -1,5 +1,5 @@
 module "worker" {
-  source            = "github.com/nubisproject/nubis-terraform//worker?ref=v1.5.0"
+  source            = "github.com/nubisproject/nubis-terraform//worker?ref=v2.0.0"
   region            = "${var.region}"
   environment       = "${var.environment}"
   account           = "${var.account}"
@@ -14,7 +14,7 @@ module "worker" {
 
 
 module "load_balancer" {
-  source       = "github.com/nubisproject/nubis-terraform//load_balancer?ref=v1.5.0"
+  source       = "github.com/nubisproject/nubis-terraform//load_balancer?ref=v2.0.0"
   region       = "${var.region}"
   environment  = "${var.environment}"
   account      = "${var.account}"
@@ -24,7 +24,7 @@ module "load_balancer" {
 }
 
 module "dns" {
-  source       = "github.com/nubisproject/nubis-terraform//dns?ref=v1.5.0"
+  source       = "github.com/nubisproject/nubis-terraform//dns?ref=v2.0.0"
   region       = "${var.region}"
   environment  = "${var.environment}"
   account      = "${var.account}"
@@ -33,7 +33,7 @@ module "dns" {
 }
 
 module "storage" {
-  source                 = "github.com/nubisproject/nubis-terraform//storage?ref=v1.5.0"
+  source                 = "github.com/nubisproject/nubis-terraform//storage?ref=v2.0.0"
   region                 = "${var.region}"
   environment            = "${var.environment}"
   account                = "${var.account}"
