5ae3b3bd6aeaa29dfa2cb05a44d14ccfebb1fc73
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-11-07 12:26:33 -0500

    log cron output somewhere

--- nubis/puppet/cron.pp
+++ nubis/puppet/cron.pp
@@ -1,16 +1,43 @@
 cron::hourly { "${project_name}-dashboard":
  user    => $apache::params::user,
- command => "nubis-cron ${project_name}-dashboard /var/www/${project_name}/dashboard.py > /var/www/${project_name}/dashboard.html",
+ command => "nubis-cron ${project_name}-dashboard /var/www/${project_name}/dashboard.py > /var/www/${project_name}/dashboard.html 2> /var/log/bzdashboard-cron.log",
+}
+
+file { '/var/log/bzdashboard-cron.log':
+  ensure  => 'present',
+  owner   => 'www-data',
+  group   => 'www-data',
+  require => [
+    Class['nubis_apache'],
+  ],
 }
 
 cron::daily { "${project_name}-bztriage":
  hour    => '3',
  user	 => 'www-data',
- command => "nubis-cron ${project_name}-bztriage /var/www/${project_name}/bz-triage.py >/dev/null 2>&1",
+ command => "nubis-cron ${project_name}-bztriage /var/www/${project_name}/bz-triage.py > /var/log/bztriage-cron.log 2>&1",
+}
+
+file { '/var/log/bztriage-cron.log':
+  ensure  => 'present',
+  owner   => 'www-data',
+  group   => 'www-data',
+  require => [
+    Class['nubis_apache'],
+  ],
 }
 
 cron::daily { "${project_name}-bznag":
  hour	 => '4',
  user    => 'www-data',
- command => "nubis-cron ${project_name}-bztriage /var/www/${project_name}/bznag.py > /dev/null 2>&1",
+ command => "nubis-cron ${project_name}-bztriage /var/www/${project_name}/bznag.py > /var/log/bznag-cron.log 2>&1",
+}
+
+file { '/var/log/bznag-cron.log':
+  ensure  => 'present',
+  owner   => 'www-data',
+  group   => 'www-data',
+  require => [
+    Class['nubis_apache'],
+  ],
 }
