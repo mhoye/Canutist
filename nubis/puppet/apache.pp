1b4eba19ed72b43fbf225bbe92ce2318b2f2dd52
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-11-07 14:18:29 -0500

    cleanup logs

--- nubis/puppet/apache.pp
+++ nubis/puppet/apache.pp
@@ -6,7 +6,7 @@
 
 class { 'nubis_apache':
   # Run this on boot to ensure we are good to go
-  update_script_source=puppet:///nubis/files/startup,
+  update_script_source => 'puppet:///nubis/files/startup',
 }
 
 file { '/var/log/bz-signup.log':
--- nubis/puppet/logs.pp
+++ nubis/puppet/logs.pp
@@ -6,14 +6,13 @@ fluentd::source { 'bznag-output':
   format      => '/^(?<time>[^\]]*) - (?<level>[^\]]*) - (?<message>.*)?$/',
   time_format => '%Y-%M-%d %H:%M:%S %Z',
 
-  tag         => 'forward.bznag.node.stdout',
+  tag         => "forward.${project_name}.bznag.stdout",
   config      => {
     'read_from_head' => true,
-    'path'           => "/var/log/${project_name}/bznag.log",
-    'pos_file'       => "/var/log/${project_name}/bznag.pos",
+    'path'           => '/var/log/bznag.log',
+    'pos_file'       => '/var/log/bznag.pos',
   },
 }
-
 
 fluentd::source { 'bztriage-output':
   configfile  => $project_name,
@@ -21,14 +20,13 @@ fluentd::source { 'bztriage-output':
   format      => '/^(?<time>[^\]]*) - (?<level>[^\]]*) - (?<message>.*)?$/',
   time_format => '%Y-%M-%d %H:%M:%S %Z',
 
-  tag         => 'forward.bztriage.node.stdout',
+  tag         => "forward.${project_name}.bztriage.stdout",
   config      => {
     'read_from_head' => true,
-    'path'           => "/var/log/${project_name}/bz-triage.log",
-    'pos_file'       => "/var/log/${project_name}/bz-triage.pos",
+    'path'           => '/var/log/bz-triage.log',
+    'pos_file'       => '/var/log/bz-triage.pos',
   },
 }
-
 
 fluentd::source { 'bzsignup-output':
   configfile  => $project_name,
@@ -36,10 +34,50 @@ fluentd::source { 'bzsignup-output':
   format      => '/^(?<time>[^\]]*) - (?<level>[^\]]*) - (?<message>.*)?$/',
   time_format => '%Y-%M-%d %H:%M:%S %Z',
 
-  tag         => 'forward.bzsignup.node.stdout',
+  tag         => "forward.${project_name}.bzsignup.stdout",
   config      => {
     'read_from_head' => true,
-    'path'           => "/var/log/${project_name}/bz-signup.log",
-    'pos_file'       => "/var/log/${project_name}/bz-signup.pos",
+    'path'           => '/var/log/bz-signup.log',
+    'pos_file'       => '/var/log/bz-signup.pos',
   },
 }
+
+fluentd::source { 'bzdashboard-cron':
+  configfile => $project_name,
+  type       => 'tail',
+  format     => 'none',
+
+  tag        => "forward.${project_name}.bzdashboard.stdout",
+  config     => {
+    'read_from_head' => true,
+    'path'           => '/var/log/bzdashboard-cron.log',
+    'pos_file'       => '/var/log/bzdashboard-cron.pos',
+  },
+}
+
+fluentd::source { 'bztriage-cron':
+  configfile => $project_name,
+  type       => 'tail',
+  format     => 'none',
+
+  tag        => "forward.${project_name}.bztriage.cron",
+  config     => {
+    'read_from_head' => true,
+    'path'           => '/var/log/bztriage-cron.log',
+    'pos_file'       => '/var/log/bztriage-cron.pos',
+  },
+}
+
+fluentd::source { 'bznag-cron':
+  configfile => $project_name,
+  type       => 'tail',
+  format     => 'none',
+
+  tag        => "forward.${project_name}.bznag.cron",
+  config     => {
+    'read_from_head' => true,
+    'path'           => '/var/log/bznag-cron.log',
+    'pos_file'       => '/var/log/bznag-cron.pos',
+  },
+}
+
